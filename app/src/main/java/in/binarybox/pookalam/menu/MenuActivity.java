7999e98c54724144adf12c86788374262a42805d
binarybox <apps.binarybox@gmail.com>
2019-08-25 23:33:10 +0530

    sound toggle button added in menu

--- app/src/main/java/in/binarybox/pookalam/menu/MenuActivity.java
+++ app/src/main/java/in/binarybox/pookalam/menu/MenuActivity.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.Toast;
 
@@ -20,7 +22,14 @@ import in.binarybox.pookalam.photo.sort.SortPhotosActivity;
 import in.binarybox.pookalam.share.ShareActivity;
 
 public class MenuActivity extends AppCompatActivity {
-    private ConstraintLayout layout1,layout2,layout3,layout4;
+    private ConstraintLayout layout1,layout2,layout3,layout4,btnSound;
+    private Boolean soundOn=true;
+    private MediaPlayer mediaPlayer;
+    private ImageView ivSoundSwitch;
+    private String SHARED_PREFS_ID = "bg_music";
+    private String SHARED_PREFS_KEY = "is_on";
+    private SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFS_ID,MODE_PRIVATE);;
+    private SharedPreferences.Editor editor= getSharedPreferences(SHARED_PREFS_ID,MODE_PRIVATE).edit();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,13 @@ public class MenuActivity extends AppCompatActivity {
         layout2=findViewById(R.id.layout2);
         layout3=findViewById(R.id.layout3);
         layout4=findViewById(R.id.layout4);
+        btnSound=findViewById(R.id.soundLayout);
+        ivSoundSwitch=findViewById(R.id.ivSoundSwitch);
+
+        setupBgMusicFn();
+        
+        getSoundModeFn();
+
 
         layout1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,16 +83,41 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        playBgMusicFn();
+        btnSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (soundOn){
+                    mediaPlayer.pause();
+                    ivSoundSwitch.setBackgroundResource(R.drawable.menu_icon_sound_off);
+                    soundOn=false;
+                    editor.putBoolean(SHARED_PREFS_KEY,soundOn);
+                }else {
+                    mediaPlayer.start();
+                    ivSoundSwitch.setBackgroundResource(R.drawable.menu_icon_sound_on);
+                    soundOn=true;
+                    editor.putBoolean(SHARED_PREFS_KEY,soundOn);
+                }
+            }
+        });
+
 
     }
 
-    private void playBgMusicFn() {
-        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.bg_music);
+    private void getSoundModeFn() {
+        soundOn=sharedPreferences.getBoolean(SHARED_PREFS_KEY,true);
+
+        if (soundOn){
+            mediaPlayer.start();
+        }else {
+            
+        }
+    }
+
+    private void setupBgMusicFn() {
+        mediaPlayer = MediaPlayer.create(this,R.raw.bg_music);
         mediaPlayer.setLooping(true);
-        mediaPlayer.start();
 
         AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 10, 0);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 5, 0);
     }
 }
